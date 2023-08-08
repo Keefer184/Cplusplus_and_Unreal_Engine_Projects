e7a0d0538e2dee31450effa2c3414943bd440d8d
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-08-07 21:06:44 -0400

    added C++ challenges

---
+++ C++Challenges/C++Challenges/C++Challenges.cpp
@@ -0,0 +1,174 @@
+
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main()
+{
+    //const int favNum = 184;
+    //string itIs = "It is ";
+    //bool isTrue = false;
+    //string favoriteNum = " my favorite number is ";
+    //double num1 = 10;
+
+    //if (isTrue == true) {
+    //    cout << itIs << "true " << favoriteNum << favNum;
+    //}
+    //else {
+    //    cout << itIs << "false " << favoriteNum << num1;
+    //}
+
+    //const int favNum = 184;
+    //double num1;
+    //cout << "Guess my favorite number?" << endl;
+    //cin >> num1;
+    //if (favNum == num1) {
+    //    cout << "Congratulations, you have guessed my favorite number!" << endl;
+
+    //}
+    //else {
+    //    cout << "Sorry, but that is not my favorite number, guess again." << endl;
+    //}
+   // float price = 19.99;
+   // string fName = "Matt";
+   // string lName = " Keefer";
+   // string fullName = fName.append(lName);
+   ///* cout << fullName << " paid " << price << " for his tickets." << endl;*/
+
+   // cout << "My full name has " << fullName.length() << " characters in it." << endl;
+   // cout << "I go by " << fullName << endl;
+   // fullName[5] = 'R';
+   // cout << "Yet sometimes kids would joke and call me " << fullName;
+
+    /*double a = 11;
+    const double b = 7;*/
+    //cout << a + b << endl;
+    //cout << a - b << endl;
+    //cout << a * b << endl;
+    //cout << a / b << endl;
+    //a++;
+    //cout << a << endl;
+    //cout << (a < b) << endl;
+    //cout << (a == b) << endl;
+    //cout << (a != b) << endl;
+    //cout << (a <= b) << endl;
+    //a = b;
+    //a += b;
+    //int d = 5;
+    //d &= 3;
+    //d <<= 3;
+   // cout << d << endl;
+    //if (a > d && b > d) {
+    //    cout << "a > d and b >d" << endl;
+    //}
+    //else {
+    //    cout << "The conditionals are not both true." << endl;
+
+    //}
+    //if (a < d || b < d) {
+    //    cout << "a < d or b < d" << endl;
+    //}
+    //else {
+    //    cout << "Neither conditionals are true." << endl;
+
+    //}
+
+    /*if (a != b) {
+        cout << "a and b are not the same value" << endl;
+    }*/
+
+    /*string fullName;
+    cout << "Type your full name please: ";
+    getline(cin, fullName);
+    cout << "Thank you " << fullName << " welcome to HELL!";*/
+
+    //int x = 10;
+    //int y = 184;
+    //cout << max(x, y) << endl;
+    //double z = sqrt(y);
+    //cout << z << endl;
+    //cout << round(z) << endl;
+    //cout << pow(x, y) << endl;
+
+    //if (x < y) {
+    //    cout << "x is less than y" << endl;
+    //}
+    //else if (x > y){
+    //    cout << "x is greater than y" << endl;
+
+    //}
+    //else {
+    //    cout << "IDK, something happened..." << endl;
+    //}
+
+    //bool isLess = (x < y);
+    //switch (isLess) {
+    //case 1:
+    //    cout << "x is less than y";
+    //    break;
+    //case 0:
+    //    cout << "x is greater than y";
+    //    break;
+    //default:
+    //    cout << "IDK, x and y are related somehow...";
+    //}
+
+    /*string result = (x < y) ? "x is less than y." : "x is greater than y.";
+    cout << result;*/
+
+    //while (x < y) {
+    //    cout << x << endl;
+    //    x++;
+    //}
+
+    //do {
+    //    cout << y << endl;
+    //    y--;
+
+    //} while (x < y);
+
+    //for (int i = 0; i < y; i++) {
+    //    x+=i;
+    //    cout << x << endl;
+    //}
+
+    //while (x < y) {
+    //    cout << x << endl;
+    //    x++;
+    //    if (x == 100) {
+    //        cout << "This is kind of ridiculous, huh?"<<endl;
+    //        continue;
+    //    }
+    //    if (x == 180) {
+    //        cout << "That's it, I'm done...";
+    //        break;
+    //    }
+
+    //}
+
+    //string heroes[3] = { "Cloud", "Tifa", "Aerith" };
+    /*cout << heroes[0];*/
+    //for (int i = 0; i < 3; i++) {
+    //    cout << heroes[i] << endl;
+    //}
+
+    //for (string hero : heroes) {
+    //    cout << hero << endl;
+    //}
+
+    int x = 184;
+    int* ptr = &x;
+    /*int& y = x;
+    cout << x << endl;
+    cout << y << endl;*/
+
+    cout << &x << endl;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+
+
+}
+
+
